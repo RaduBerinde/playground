d42b712bc56b06b3c012b9e36c9322c2b31b7ee3
Radu Berinde <radu@cockroachlabs.com>
2017-02-16 12:26:36 -0500

    chan test w/meta

--- chan_test/main_test.go
+++ chan_test/main_test.go
@@ -46,33 +46,6 @@ func BenchmarkChan1(b *testing.B) {
 	close(c)
 }
 
-// Variant 2: two channels
-
-func receiverChan2(dataChan <-chan []int, errChan <-chan error) {
-	for {
-		select {
-		case d := <-dataChan:
-			for x := range d {
-				sum += x
-			}
-		case <-errChan:
-			return
-		}
-	}
-}
-
-func BenchmarkChan2(b *testing.B) {
-	dataChan := make(chan []int, chanBuf)
-	errChan := make(chan error)
-	go receiverChan2(dataChan, errChan)
-	d := make([]int, 1)
-	for i := 0; i < b.N; i++ {
-		d[0] = i
-		dataChan <- d
-	}
-	close(errChan)
-}
-
 // Variant 1a: single channel with multi-purpose message, plus ticker channel
 
 func receiverChan1a(c <-chan chanMsg, t <-chan time.Time) {
@@ -102,6 +75,33 @@ func BenchmarkChan1a(b *testing.B) {
 	close(c)
 }
 
+// Variant 2: two channels
+
+func receiverChan2(dataChan <-chan []int, errChan <-chan error) {
+	for {
+		select {
+		case d := <-dataChan:
+			for x := range d {
+				sum += x
+			}
+		case <-errChan:
+			return
+		}
+	}
+}
+
+func BenchmarkChan2(b *testing.B) {
+	dataChan := make(chan []int, chanBuf)
+	errChan := make(chan error)
+	go receiverChan2(dataChan, errChan)
+	d := make([]int, 1)
+	for i := 0; i < b.N; i++ {
+		d[0] = i
+		dataChan <- d
+	}
+	close(errChan)
+}
+
 // Variant 2a: two channels, plus ticker channel
 
 func receiverChan2a(dataChan <-chan []int, errChan <-chan error, t <-chan time.Time) {
@@ -129,4 +129,67 @@ func BenchmarkChan2a(b *testing.B) {
 		dataChan <- d
 	}
 	close(errChan)
+}
+
+type chanMeta struct {
+	ranges []int
+	err    error
+}
+
+type chanMetaMsg struct {
+	data []int
+	meta chanMeta
+}
+
+// Variant 3: single channel with larger metadata
+
+func receiverChan3(c <-chan chanMetaMsg) {
+	for m := range c {
+		for x := range m.data {
+			sum += x
+		}
+		if m.meta.err != nil {
+			return
+		}
+	}
+}
+
+func BenchmarkChan3(b *testing.B) {
+	c := make(chan chanMetaMsg, chanBuf)
+	go receiverChan3(c)
+	d := make([]int, 1)
+	for i := 0; i < b.N; i++ {
+		d[0] = i
+		c <- chanMetaMsg{d, chanMeta{}}
+	}
+	close(c)
+}
+
+// Variant 3a: single channel with larger metadata + ticker channel
+
+func receiverChan3a(c <-chan chanMetaMsg, t <-chan time.Time) {
+	for {
+		select {
+		case m := <-c:
+			for x := range m.data {
+				sum += x
+			}
+			if m.meta.err != nil {
+				return
+			}
+		case <-t:
+		}
+	}
+}
+
+func BenchmarkChan3a(b *testing.B) {
+	t := time.Tick(100 * time.Microsecond)
+	c := make(chan chanMetaMsg, chanBuf)
+	go receiverChan3a(c, t)
+	d := make([]int, 1)
+	for i := 0; i < b.N; i++ {
+		d[0] = i
+		c <- chanMetaMsg{d, chanMeta{}}
+	}
+	close(c)
 }
