c466736ccd91a937da0941a3c27c7f318c7c9cbe
Radu Berinde <radu@cockroachlabs.com>
2017-07-20 08:51:39 -0400

    escape test
    We can't ever get a pointer to something on the stack, even with unsafe.

--- escape-test/main.go
+++ escape-test/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"unsafe"
+)
+
 var f func()
 
 func cucu() {
@@ -11,4 +16,13 @@ func main() {
 	cucu()
 	f()
 	f()
+	a := 5
+	x := unsafe.Pointer(&a)
+	fmt.Printf("%d\n", x)
+
+	b := [2]int{1, 2}
+	c := b[:1]
+	c[1] = 3
+	y := unsafe.Pointer(&c)
+	fmt.Printf("%d\n", b, y)
 }
